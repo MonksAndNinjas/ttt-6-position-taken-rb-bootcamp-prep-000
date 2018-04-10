b32af089905976f3e14ff63a8eff1551e1e2b7a4
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-10 20:09:35 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,9 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     false
-  elsif board[index] == "X" || "O"
-    true
-  else
+  elsif board[index] != "X" || "O"
     false
+  else
+    true
   end
 end
