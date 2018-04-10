98511b1908c6440c0b3f7076b76c43921ffb2892
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-10 19:51:24 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != "X" || "O"
+  if board[index] != " " || "" || nil
     true
-  elsif board[index] != " "
+  else
     false
   end
 end
